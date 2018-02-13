b3ba2216f62b74290aa3542ce3dc488aa6b9ff9a
Dennis Medlock <denmedlock@gmail.com>
2018-02-13 04:22:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,10 @@
 def display_board
-  phrase = "Tic Tac Toe board"
-  end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+  puts "Tic Tac Toe board"
+  puts    |   |
+  puts -----------
+  puts    |   |   
+  puts -----------
+  puts    |   |   
+end
+    
+  
