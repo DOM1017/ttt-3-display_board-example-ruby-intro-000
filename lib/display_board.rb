50dde8f95216f4479b8eb705973bbca449d35692
Dennis Medlock <denmedlock@gmail.com>
2018-02-13 04:27:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
   puts "Tic Tac Toe board"
-  puts    |   |
-  puts -----------
-  puts    |   |
-  puts -----------
-  puts    |   |
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
